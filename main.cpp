fb0c395ce5788ac6924a66323d2a31a79c115824
Vishwah <vishwahsivagurunathan@gmail.com>
2026-09-19 21:16:46 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include "tgaimage.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green   = {  0, 255,   0, 255};
+constexpr TGAColor red     = {  0,   0, 255, 255};
+constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor yellow  = {  0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep) { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx) { // make it left−to−right
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++) {
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
+        ierror += 2 * std::abs(by - ay);
+        if (ierror > bx - ax) {
+            y += by > ay ? 1 : -1;
+            ierror -= 2 * (bx - ax);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    constexpr int width  = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    int ax =  7, ay =  3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    line(ax,ay,bx,by,framebuffer,yellow);
+    line(bx,by,cx,cy,framebuffer,blue);
+    line(cx,cy,ax,ay,framebuffer,red);
+
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
+}
+
